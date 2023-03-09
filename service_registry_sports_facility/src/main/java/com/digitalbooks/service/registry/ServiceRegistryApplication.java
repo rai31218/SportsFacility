a591083c38833fb6726f09c987dfc8387c8e0131
rai31218 <deboparna.chakraborty@gmail.com>
2023-03-09 19:48:33 +0530

    Docker added

--- service_registry_sports_facility/src/main/java/com/digitalbooks/service/registry/ServiceRegistryApplication.java
+++ service_registry_sports_facility/src/main/java/com/digitalbooks/service/registry/ServiceRegistryApplication.java
@@ -10,6 +10,8 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
+		String myStr = "Hello planet earth, you are a great planet.";
+		System.out.println(myStr.indexOf("e", 5));
 	}
 
 }
